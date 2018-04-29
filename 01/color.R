bebeca95c8e6f6a21a64e42f847ca889bdea9153
Julia Böhlke <julia@Julias-MBP.fritz.box>
2018-04-29 11:02:29 +0200

    img.e -> img.f umbenannt

--- 01/color.R
+++ 01/color.R
@@ -2,6 +2,7 @@
 
 load (file='color.rda')
 plot(rlogo)
+#pdf(file = 'color.pdf')
 #plot(0,0, type='n', xaxt='n', yaxt='n', xlab='', ylab='', bty='n') 
 
 #Vektor mit Intensittswerten
@@ -10,12 +11,12 @@ intensity <- seq(0,1,length.out = 25)
 ##### a #####
 img.a <- array(0,dim = c(25,25,3))
 img.a[1:25,1:25,1:3] <- intensity[1:25]
-plot.array(img.a)
+plot.array(img.a, main = "a) Graustufenbild")
 
 ###### b #####
 img.b <- array(0,dim = c(25,25,3))
 for (i in seq(1:25)) img.b[i,25:1,2] <- intensity[1:25]
-plot.array(img.b)
+plot.array(img.b, main = "b) Vertikal aufsteigende Grünwerte" )
 
 ##### c #####
 img.c  <- array(0,dim = c(25,25,3))
@@ -23,53 +24,41 @@ img.c  <- array(0,dim = c(25,25,3))
 img.c[25:1,1:25,3] <- intensity[1:25]
 #rot vertikal austeigend
 for (i in seq(1:25)) img.c[i,1:25,1] <- intensity[1:25]
-plot.array(img.c)
+plot.array(img.c, main = "c) Rot und Blau aufsteigende Farbwerte")
 
 ##### d #####
-# gespiegelt ????
+
 img.d <- img.c
 img.d[1:25,1:25,2] <- 1-(img.d[1:25,1:25,1] + img.d[1:25,1:25,3])
 img.d[img.d[1:25,1:25,2]>1] <- 1 
 img.d[img.d[1:25,1:25,2]<0] <- 0
-plot.array(img.d)
-#####e#####
-# (e) Heben Sie die Farbwerte in (d) multiplikativ an, so dass der höchste Farbwert 1 ist.
+plot.array(img.d, main = "d) Addierte Grünwerte ")
+
+##### e #####
+
 img.e <-img.d
 for (i in seq(1:25)){
   for (j in seq(1:25)){
-    r<-img.e[i,j,1] 
-    b<-img.e[i,j,2] 
-    g<-img.e[i,j,3] 
-    print(paste("before rbg: ", r, b, g))
-    img.e[i,j,1]<- round(img.e[i,j,1], 4)
-    img.e[i,j,2]<- round(img.e[i,j,2], 4)
-    img.e[i,j,3]<- round(img.e[i,j,3], 4)
-    r<-img.e[i,j,1] 
-    b<-img.e[i,j,2] 
-    g<-img.e[i,j,3] 
-    print(paste("rounded rbg: ", r, b, g))
 max <-img.e[i,j,1]
-
-#print(paste("max: " , max))
-if(img.e[i,j,2] > img.e[i,j,1]) { max<-img.e[i,j,2]
-print(paste("max: ", max))}
-
-if(img.e[i,j,3] > img.e[i,j,2]) max<-img.e[i,j,3]
+if(img.e[i,j,2] > img.e[i,j,1]) max<-img.e[i,j,2]
+if(img.e[i,j,3] > max) max<-img.e[i,j,3]
 factor<-1
 if (max!=0) factor<- (1/max)
 img.e[i,j,1:3]<-(img.e[i,j,1:3]*factor)
-#img.e[img.e[1:25,1:25,1:3]>1] <- 1 
-r<-img.e[i,j,1] 
-b<-img.e[i,j,2] 
-g<-img.e[i,j,3] 
-print(paste(i, j, " maximum: ", max ,factor," rbg: ",r, b ,g))
-print(r)
 }}
+plot.array(img.e, main = "e) Multiplikativ angehobene Farbwerte")
 
-plot.array(img.e)
+##### f #####
 
-
-
-#####f#####
-# (f) Heben Sie die Farbwerte in (d) additiv an, so dass der höchste Farbwert 1 ist.
-
+img.f <-img.d
+for (i in seq(1:25)){
+  for (j in seq(1:25)){
+    max <-img.f[i,j,1]
+    if(img.f[i,j,2] > img.f[i,j,1]) max<-img.f[i,j,2]
+    if(img.f[i,j,3] > max) max<-img.f[i,j,3]
+    differenz <-0
+    if(max!=0) differenz<- 1-max
+    img.f[i,j,1:3]<-(img.f[i,j,1:3]+differenz)
+  }}
+plot.array(img.e, main = "f) Addititv angehobene Farbwerte")
+#dev.off()
